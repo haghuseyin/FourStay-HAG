f64b05c3a857aab3bda8e4e8228f8bfa23b04ef1
rahib520 <asadovnq@gmail.com>
2017-08-29 15:18:43 -0400

    I did big change in cuckes runner!!!

--- src/test/java/com/fourstay/runners/CukesRunner.java
+++ src/test/java/com/fourstay/runners/CukesRunner.java
@@ -6,15 +6,15 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-				plugin={"html:target/cucumber-reports", "json:target/cucumber.json"},
-				features = "src/test/resources/features", 
-				glue = "com/fourstay/step_definitions", 
-				tags = "@Assignment"
-				,dryRun=false
-				)
+@CucumberOptions(plugin = { "html:target/cucumber-reports",
+		"json:target/cucumber.json" }, features = "src/test/resources/features", glue = "com/fourstay/step_definitions", tags = "@Assignment", dryRun = false)
 public class CukesRunner {
-	// My name is CAgri and I am adding this comment to show that I know how to update 
-	//a java project and push and commit to GitHub back. 
-	
+	// My name is CAgri and I am adding this comment to show that I know how to
+	// update
+	// a java project and push and commit to GitHub back.
+
+	/*
+	 * I can do it too
+	 */
+
 }
